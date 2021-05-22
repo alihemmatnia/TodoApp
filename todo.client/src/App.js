acf7999af811342f7bd2c5b581536100a566182e
Ali <ali.linux2004h@gmail.com>
2021-05-22 19:17:11 +0430

    Some Edit

--- todo.client/src/App.js
+++ todo.client/src/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import axios from 'axios';
 import swal from 'sweetalert';
+
 class App extends React.Component {
     constructor() {
         super();
@@ -9,6 +10,11 @@ class App extends React.Component {
         this.state = {
             todos: [], title: "",
         }
+
+        this.TitleChange=this.TitleChange.bind(this);
+        this.ChangeStatus=this.ChangeStatus.bind(this);
+        this.AddTodo=this.AddTodo.bind(this);
+        this.DeleteTodo=this.DeleteTodo.bind(this);
     }
     
     componentDidMount() {
@@ -17,32 +23,33 @@ class App extends React.Component {
 
     // Refresh Data If Update In Todos
     componentDidUpdate(){
-        axios.get(`${this.baseUrl}/Api/todo`).then(p => this.setState({ todos: p.data.todos }))
+        axios.get(`${this.baseUrl}/Api/todo`).then(p => this.setState({ todos: p.data.todos })).catch(e=>swal("خطا", "خطایی رخ داد بعدا تلاش کنید","error"))
     }
 
     // Add A Todo
-    AddTodo = () => {
+    AddTodo(e) {
+        e.preventDefault();
         if (this.state.title === "") {
             swal("خطا", "نباید عنوان خالی باشد", "error");
         }
         else{
-            axios.post(`${this.baseUrl}/Api/todo`, { 'title': this.state.title }).then(p => swal("موفقیت","با موفقیت افزوده شد","success"))
+            axios.post(`${this.baseUrl}/Api/todo`, { 'title': this.state.title }).then(p => swal("موفقیت","با موفقیت افزوده شد","success")).catch(e=>swal("خطا", "خطایی رخ داد بعدا تلاش کنید","error"))
         }
     }
 
     // Change Status(If true Changed To false Or )
-    ChangeStatus = (id) => {
-        axios.put(`${this.baseUrl}/api/todo/${id}`).then(e => swal("موفقیت","با موفقیت وضعیت تغییر کرد","success"));
+    ChangeStatus(id) {
+        axios.put(`${this.baseUrl}/api/todo/${id}`).then(e => swal("موفقیت","با موفقیت وضعیت تغییر کرد","success")).catch(e=>swal("خطا", "خطایی رخ داد بعدا تلاش کنید","error"))
     }
 
     // Handle Change Title Value
-    TitleChange = (e) => {
+    TitleChange(e) {
         this.setState({ title: e.target.value })
     }
 
     // Delete Todo With  Id
     DeleteTodo(id) {
-        axios.delete(`${this.baseUrl}/Api/todo/${id}`).then(p =>swal("موفق","با موفقیت حذف شد", "warning"));
+        axios.delete(`${this.baseUrl}/Api/todo/${id}`).then(p =>swal("موفق","با موفقیت حذف شد", "warning")).catch(e=>swal("خطا", "خطایی رخ داد بعدا تلاش کنید","error"))
     }
 
     // render Page
@@ -51,8 +58,11 @@ class App extends React.Component {
             <div className="App container">
                 <div  className="header">
                     <h3>کار های من</h3>
-                    <input onChange={this.TitleChange} type="text"  className="form-control" placeholder="عنوان"></input>
-                    <span onClick={this.AddTodo} className="btn btn-primary">اضافه کردن</span>
+                   <form onSubmit={(e)=>this.AddTodo(e)}>
+                   <input onChange={this.TitleChange} type="text"  className="form-control" placeholder="عنوان"></input>
+                   <br></br>
+                    <input type="submit" className="btn btn-primary" value="افزودن"></input>
+                   </form>
                 </div>
 
                 <table className="table table-striped table-hover ">
